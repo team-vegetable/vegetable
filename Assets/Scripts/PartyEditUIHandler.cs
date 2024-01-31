f488a5313e5b22277d8cd4c2250c9eb4517d12c3
YamanaSousuke <sssosuke0119@gmail.com>
2024-01-31 14:20:15 +0900

    QuickSaveを使用して保存と取得を行うように修正

--- Assets/Scripts/PartyEditUIHandler.cs
+++ Assets/Scripts/PartyEditUIHandler.cs
@@ -3,10 +3,15 @@ using UnityEngine;
 using System.Linq;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using CI.QuickSave;
+using UnityEditor;
 
 //　パーティー編成画面のUIの管理
 public class PartyEditUIHandler: MonoBehaviour
 {
+    // 野菜のアイコンのプレハブ
+    [SerializeField] private UnitIcon vegetableIcon = null;
+
     // バトルシーンに遷移させるボタン(テスト用)
     [SerializeField] private Button transitionBattleSceneButton = null;
     // 保存ボタン
@@ -15,10 +20,6 @@ public class PartyEditUIHandler: MonoBehaviour
     [SerializeField] private Transform mainVegetablesParent = null;
     // サブの野菜を格納する親オブジェクト
     [SerializeField] private Transform reserveVegetablesParent = null;
-
-    // 戦闘に使用する野菜を格納する
-    private readonly List<UnitIcon> mainVegetableIcons = new();
-    private readonly List<Vegetable> mainVegetables = new();
 
     private readonly List<GameObject> mainVegetableObjects = new();
     private readonly List<GameObject> reserveVegetableObjects = new();
@@ -30,30 +31,38 @@ public class PartyEditUIHandler: MonoBehaviour
         transitionBattleSceneButton.onClick.RemoveAllListeners();
         transitionBattleSceneButton.onClick.AddListener(OnClickTransitionBattleSceneButton);
 
+        QuickSaveSettings settings = new() {
+            //SecurityMode = SecurityMode.Aes,
+            //Password = "Password",
+            //CompressionMode = CompressionMode.Gzip,
+        };
+
+        // 保存データ読み込み
+        QuickSaveReader reader = QuickSaveReader.Create("PartyData", settings);
+        var mainVegetableIDs = reader.Read<List<int>>("MainVegetableIDs");
+
+        // メインの野菜アイコンの生成
+        var vegtableAssets = LoadScriptableObjectFromFolder<Vegetable>("Assets/ScriptableObjects");
+        foreach (var id in mainVegetableIDs) {
+            var asset = vegtableAssets.FirstOrDefault(e => e.ID == id);
+            if (asset == null) {
+                continue;
+            }
+            var icon = Instantiate(vegetableIcon, mainVegetablesParent);
+            icon.Init(asset, SwitchIcon);
+        }
+
+        // メインの野菜オブジェクトを取得
         foreach (Transform child in mainVegetablesParent.transform) {
             mainVegetableObjects.Add(child.gameObject);
         }
-
+        // サブの野菜オブジェクトを取得
         foreach (Transform child in reserveVegetablesParent.transform) {
             reserveVegetableObjects.Add(child.gameObject);
         }
-
-        foreach (Transform child in mainVegetablesParent.transform) {
-            var icon = child.GetComponent<UnitIcon>();
-            icon.Init(SwitchIcon);
-            mainVegetableIcons.Add(icon);
-        }
-
-        foreach (Transform child in reserveVegetablesParent.transform) {
-            var icon = child.GetComponent<UnitIcon>();
-            if (icon == null) {
-                continue;
-            }
-            icon.Init(SwitchIcon);
-            mainVegetableIcons.Add(icon);
-        }
     }
 
+    // アイコンの入れ替え
     private void SwitchIcon(GameObject moveIcon, GameObject hitIcon) {
         // サブ同士の入れ替えは受け付けない(上手く動かない)
         if (reserveVegetableObjects.Any(e => e == moveIcon) && reserveVegetableObjects.Any(e => e == hitIcon)) {
@@ -101,17 +110,44 @@ public class PartyEditUIHandler: MonoBehaviour
     // 保存ボタンを押したとき
     private void OnClickSaveButton() {
         // 戦闘に使用する野菜の取得
+        // List<int> mainVegetableIDs = new();
+        // テスト用のデータ
+        List<int> mainVegetableIDs = new() { 1, 2, 3 };
         foreach (Transform child in mainVegetablesParent.transform) {
-            var vegetable = child.GetComponent<UnitIcon>().Vegetable;
-            mainVegetables.Add(vegetable);
+            var id = child.GetComponent<UnitIcon>().Vegetable.ID;
+            // mainVegetableIDs.Add(id);
         }
 
-        // 他のシーンでも使用できるように保存
-        GameController.Instance.SetMainVegetables(mainVegetables);
+        // 戦闘に使用する野菜の保存
+        QuickSaveSettings settings = new() {
+            //SecurityMode = SecurityMode.Aes,
+            //Password = "Password",
+            //CompressionMode = CompressionMode.Gzip,
+        };
+
+        QuickSaveWriter writer = QuickSaveWriter.Create("PartyData", settings);
+        writer.Write("MainVegetableIDs", mainVegetableIDs);
+        writer.Commit();
     }
 
     // バトルシーンに遷移させるボタンを押したとき(テスト用)
     private void OnClickTransitionBattleSceneButton() {
         SceneManager.LoadScene("BattleScene");
+    }
+
+    private List<T> LoadScriptableObjectFromFolder<T>(string folderPath) where T : ScriptableObject {
+        string[] guids = AssetDatabase.FindAssets($"t:{typeof(T).Name}", new[] { folderPath });
+        if (guids.Length <= 0) {
+            Debug.LogError("パスの指定が間違っています");
+            return null;
+        }
+
+        List<T> vegetables = new();
+        foreach (var guid in guids) {
+            string assetPath = AssetDatabase.GUIDToAssetPath(guid);
+            var asset = AssetDatabase.LoadAssetAtPath<T>(assetPath);
+            vegetables.Add(asset);
+        }
+        return vegetables;
     }
 }
