d79bfc834178ca3fe090157bae8f588673a5b30e
YamanaSousuke <sssosuke0119@gmail.com>
2024-02-21 15:44:47 +0900

    敵の生成処理と野菜を狙う処理の追加

--- Assets/Scripts/BattleSceneManager.cs
+++ Assets/Scripts/BattleSceneManager.cs
@@ -8,9 +8,13 @@ public class BattleSceneManager : MonoBehaviour
     [SerializeField] private BattleUIHandler battleUIHandler = null;
     // 戦闘に使用する野菜を格納する親オブジェクト
     [SerializeField] private Transform mainVegetablesParent = null;
+    // 3体の野菜の座標
+    [SerializeField] private List<Transform> vegetablePositions = null;
 
     // 生成する敵
     [SerializeField] private GameObject prefab = null;
+    // 敵を生成する座標
+    [SerializeField] private Transform generatePosition = null;
     // 生成する敵の親オブジェクト
     [SerializeField] private Transform parent = null;
 
@@ -19,13 +23,21 @@ public class BattleSceneManager : MonoBehaviour
     // 生成するインターバル
     [SerializeField] private int interval = 0;
 
+    // 連続で生成したときのYオフセット(重ならないようにするため)
+    [SerializeField] private float offsetY = 0.0f;
+
     private float timer = 1.0f;
     // 戦闘に使用する野菜を格納する
     private List<GameObject> mainVegetables = new();
 
+    // 手前の野菜を攻撃している動物の数
+    private int frontAnimalsCount = 0;
+
+    private const int MAX_SORTING_ORDER = 100;
+
     private void Start() {
-        // battleUIHandler.SetCountText(count);
-        // 
+        battleUIHandler.SetCountText(count);
+
         // foreach (Transform child in mainVegetablesParent.transform) {
         //     mainVegetables.Add(child.gameObject);
         // }
@@ -40,6 +52,16 @@ public class BattleSceneManager : MonoBehaviour
     }
 
     private void Update() {
+        if (Input.GetKeyDown(KeyCode.Return)) {
+            // 動物の生成
+            var animal = Instantiate(prefab, generatePosition.transform.position, Quaternion.identity, parent).GetComponent<Enemy>();
+
+            // TODO : とりあえず人参をめがけて移動しているので後程変更
+            var targetPosition = new Vector2(vegetablePositions[0].position.x, vegetablePositions[0].position.y + frontAnimalsCount * offsetY);
+            animal.Init(targetPosition, MAX_SORTING_ORDER - frontAnimalsCount);
+            frontAnimalsCount++;
+        }
+
         // if (count <= 0) {
         //     return;
         // }
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,12 +7,65 @@ public class Enemy : MonoBehaviour
 {
     // 移動スピード
     [SerializeField] private int speed = 1;
-    // 生成されたとき
-    //public void Generated() {
+    // 攻撃範囲
+    [SerializeField] private int attackRange = 1;
 
-    //}
+    // スプライトレンダラー
+    private SpriteRenderer spriteRenderer = null;
+    // 初期座標
+    private Vector2 initPosition = new();
+    // 攻撃する野菜の座標
+    private Vector2 target = new();
+
+    private bool isDying = false;
+
+    private enum State {
+        // 狙う
+        Target,
+        // 攻撃
+        Attack,
+        // 瀕死
+        Dying,
+    }
+    private State state = State.Target;
+
+    // 初期化
+    public void Init(Vector2 target, int sortingOrder) {
+        this.target = target;
+
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sortingOrder = sortingOrder;
+        initPosition = transform.position;
+    }
 
     private void Update() {
-        transform.position += new Vector3(-1.0f * speed * Time.deltaTime, 0.0f, 0.0f);
+        // ターゲットが指定されていなければ移動しない
+        if (target == Vector2.zero) {
+            return;
+        }
+
+        Vector2 currentPosition = transform.position;
+        Vector2 direction = target - currentPosition;
+        if (direction.magnitude <= attackRange && !isDying) {
+            isDying = true;
+            state = State.Attack;
+            StartCoroutine(OnDeadTemp());
+            return;
+        }
+
+        // TODO : 移動についてベストのものを選択する
+        if (state != State.Attack) {
+            if (state == State.Dying) {
+                direction = initPosition - currentPosition;
+            }
+            transform.Translate(speed * Time.deltaTime * direction.normalized);
+        }
+    }
+    
+    // 仮の死亡処理
+    private IEnumerator OnDeadTemp() {
+        yield return new WaitForSeconds(2.0f);
+        state = State.Dying;
+        spriteRenderer.flipX = !spriteRenderer.flipX;
     }
 }
